422a59a241276e7bbc3ac8b1721fb0640e51b8bb
tonym <tony@fbpllc.com>
2024-04-12 08:13:10 -0700

    add component library and root layout

---
+++ src/layouts/RootLayout/RootLayout.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { createTheme, ThemeProvider } from '../../components/dist';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif'
+  }
+});
+
+export function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
+}
